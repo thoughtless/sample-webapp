02e29ec0fd8481a3089cb4624db3bbc84666c033
Andy Shinn <andys@andyshinn.as>
2014-12-29 13:56:53 -0600

    fix redis counter

--- webapp/app.py
+++ webapp/app.py
@@ -6,16 +6,16 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     try:
-        client = etcd.Client(host=os.environ.get('ETCD_HOST', '172.17.42.1'))
-        key = client.read('/app/services/redis').value
-        redis_url = client.get(key).value
-        count = redis.StrictRedis.from_url("redis://{0}/0").format(redis_url).incr("counter")
+        client = etcd.Client(host=os.environ.get('ETCD_HOST', '10.1.42.1'))
+        key = str(client.read('/app/services/redis')._children[0]['value'])
+        redis_url = 'redis://{0}/0'.format(key)
+        count = redis.StrictRedis.from_url(redis_url).incr("counter")
     except:
         count = "redis not found"
     return "\n".join([
         '<div style="text-align: center; font-size: 128px;">Hello Docker Austin!</div>',
-        '<div style="text-align: center; font-size: 64px;">Count: {0}</div>'.format(count),
-        '<div style="text-align: center; font-size: 64px;">{0}</div>\n'.format(socket.gethostname())
+        '<div style="text-align: center; font-size: 64px;">Redis Counter: {0}</div>'.format(count),
+        '<div style="text-align: center; font-size: 64px;">Container: {0}</div>\n'.format(socket.gethostname())
     ])
 
 if __name__ == '__main__':
